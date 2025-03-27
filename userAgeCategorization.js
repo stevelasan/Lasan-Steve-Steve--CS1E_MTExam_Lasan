e1eb3a0b7e1dc9d6f3faa5b5e64dbcdf6965224e
stevelasan <stevelasan31@gmail.com>
2025-03-27 10:12:45 +0800

    Create userAgeCategorization.js

---
+++ userAgeCategorization.js
@@ -0,0 +1,35 @@
+let age = prompt("Please enter your age:")
+
+if (age <= 5){
+    console.log("You are a toddler.");
+}
+if (age >=5-12){
+    console.log("You are a child");
+}
+if (age >=13-19){
+    console.log("You are a teenager");        
+}
+if (age >=20-35){
+    console.log("You are a young adult");
+}
+if (age >=36-60){
+    console.log("You are a middle-aged");
+} else {
+    console.log("You are a senior")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
