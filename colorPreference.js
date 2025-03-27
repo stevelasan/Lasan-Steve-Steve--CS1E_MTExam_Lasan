ce058819e7a49a8c0ed97c13b034fbc1206dbd43
stevelasan <stevelasan31@gmail.com>
2025-03-27 10:15:09 +0800

    Create colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,13 @@
+let colors = prompt("Enter your favorite color:");
+let numberOfColors = 3;
+
+for (let i = 0; i< numberOfColors; i++){
+    let colors = prompt("Enter a color you like:");
+    colors.push(color);
+    console.log("Updated color list:", colors);
+}
+
+
+
+
+
