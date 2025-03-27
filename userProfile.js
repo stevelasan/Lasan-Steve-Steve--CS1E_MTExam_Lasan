2f8c2866cd6107ea53095bee100a85274edb045c
stevelasan <stevelasan31@gmail.com>
2025-03-27 10:10:05 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+let fullName = prompt("Please enter your full name (first and last name):")
+let age = prompt("Please enter your age:");
+let favoriteNumber = prompt("Please enter your favorite number:");
+let favoriteColor = prompt("Please enter your favorite color:");
+
+console.log("User Profile:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
